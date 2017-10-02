c736b09bcfc98d3ae62547ac0a203bdd689aad04
iacopomarri14 <iacopomarri@gmail.com>
2017-10-03 00:59:23 +0200

    First commit

---
+++ app/src/main/java/com/example/user/megapplicazione/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.user.megapplicazione;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private int prova;
+
+    private Toolbar mToolbar;
+
+    //PER IL FRAGMENT HOME
+    FragmentTransaction fragmentTransaction;
+    //
+    NavigationView navigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar =(Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //GESTIONE DEL FRAGMENT HOME (PAGINA PRINCIPALE)
+        fragmentTransaction= getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new HomeFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("Home Fragment");
+
+        //NAVIGAZIONE TRA ITEMS DEL NAVIGATION DRAWER
+        navigationView =(NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_home:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new HomeFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home Fragment");
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_settings:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new SettingsFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Settings Fragment");
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_pics:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new PicsFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Pics Fragment");
+                       // item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_maintenance:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new MaintenanceFragment());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Maintenance Fragment");
+                        mDrawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.nav_veichles:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new VeichlesFragments());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Veichles Fragment");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                }
+                return true ;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
---
+++ app/src/main/java/com/example/user/megapplicazione/VeichlesFragments.java
@@ -0,0 +1,30 @@
+
+package com.example.user.megapplicazione;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VeichlesFragments extends Fragment {
+
+
+    public VeichlesFragments() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_veichles_fragments, container, false);
+    }
+
+}
