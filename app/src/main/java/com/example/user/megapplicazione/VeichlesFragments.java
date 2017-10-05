be6e71c67bf8df337d8d0968f16407faf5bcb2b0
iacopomarri14 <iacopomarri@gmail.com>
2017-10-05 23:09:44 +0200

    Changed color, added home icon.

--- app/src/main/java/com/example/user/megapplicazione/VeichlesFragments.java
+++ app/src/main/java/com/example/user/megapplicazione/VeichlesFragments.java
@@ -24,7 +24,7 @@ public class VeichlesFragments extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_veichles_fragments, container, false);
+        return inflater.inflate(R.layout.fragment_veichles, container, false);
     }
 
 }
